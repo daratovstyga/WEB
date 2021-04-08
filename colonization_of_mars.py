37263ced81a784603295e923231805385929801d
daratovstyga <daratovstyga3536@gmail.com>
2021-04-08 18:16:43 +0300

    Изображение Марса

---
+++ colonization_of_mars.py
@@ -0,0 +1,43 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mission_name():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    p_list = ['Человечество вырастает из детства.',
+              'Человечеству мала одна планета.',
+              'Мы сделаем обитаемыми безжизненные пока планеты.',
+              'И начнем с Марса!',
+              'Присоединяйся!']
+    return "</br>".join(p_list)
+
+
+@app.route('/image_mars')
+def image():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars-planet-clip-art-15.png')}" width= "400" height="400">
+                        <p>Вот она какая, красная планета.</p>
+                      </body>
+                    </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
