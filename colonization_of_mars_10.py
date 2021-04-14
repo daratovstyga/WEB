a056c2edfc52081ffada6660c8576b841e23228a
daratovstyga <daratovstyga3536@gmail.com>
2021-04-14 17:57:08 +0300

    Список профессий

---
+++ colonization_of_mars_10.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof.lower())
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    return render_template('list_prof.html', prof=professions, type=list)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
